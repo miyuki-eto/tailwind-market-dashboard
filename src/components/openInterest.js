222ddd90f82de58424d3ff6397508d26fbb2e767
Miyuki Eto <miyukietocrypt@gmail.com>
2021-07-29 17:19:45 +1000

    initial

--- src/components/openInterest.js
+++ src/components/openInterest.js
@@ -37,21 +37,42 @@ export default function OpenInterest() {
         <div className="px-4 py-8 rounded-3xl max-w-full text-white bg-gray-800 shadow-inner">
             <h2 className="text-2xl font-medium mb-10 pl-1 md:pl-3">Open Interest</h2>
 
-            <div className="flex flex-row items-center py-4 px-4 rounded-b rounded-2xl max-w-full text-white bg-gray-900 border-b border-gray-600 shadow-lg">
-                <div
-                    className="text-md font-medium w-full grid grid-flow-col grid-cols-7 justify-items-end">
-                    <p>{" "}</p>
-                    <p>Token</p>
-                    <p>OI (USD)</p>
-                    <p>OI (Token)</p>
-                    <p>1H Change</p>
-                    <p>4H Change</p>
-                    <p>24H Change</p>
-                </div>
-            </div>
-            {dataOiALL.map(token => (
-                <OpenInterestCard key={token.symbol} token={token} exchangeData={dataOiEX.filter(row => row.symbol === token.symbol)}/>
-            ))}
+            <table className="table-auto">
+                <thead>
+                <tr>
+                    <th>{" 1"}</th>
+                    <th>{" 2"}</th>
+                    <th>Symbol</th>
+                    <th>Token</th>
+                    <th>OI (USD)</th>
+                    <th>OI (Token)</th>
+                    <th>1H Change</th>
+                    <th>4H Change</th>
+                    <th>24H Change</th>
+                </tr>
+                </thead>
+                <tbody>
+                {dataOiALL.map(token => (
+                    <OpenInterestCard key={token.symbol} token={token} exchangeData={dataOiEX.filter(row => row.symbol === token.symbol)}/>
+                ))}
+                </tbody>
+            </table>
+
+            {/*<div className="flex flex-row items-center py-4 px-4 rounded-b rounded-2xl max-w-full text-white bg-gray-900 border-b border-gray-600 shadow-lg">*/}
+            {/*    <div*/}
+            {/*        className="text-md font-medium w-full grid grid-flow-col grid-cols-7 justify-items-end">*/}
+            {/*        <th>{" "}</th>*/}
+            {/*        <th>Token</th>*/}
+            {/*        <th>OI (USD)</th>*/}
+            {/*        <th>OI (Token)</th>*/}
+            {/*        <th>1H Change</th>*/}
+            {/*        <th>4H Change</th>*/}
+            {/*        <th>24H Change</th>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
+            {/*{dataOiALL.map(token => (*/}
+            {/*    <OpenInterestCard key={token.symbol} token={token} exchangeData={dataOiEX.filter(row => row.symbol === token.symbol)}/>*/}
+            {/*))}*/}
 
         </div>
     );
